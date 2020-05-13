999d721cbcb2c1f2eb9725dd183fdd0d19518915
DevilJin23 <45466047+DevilJin23@users.noreply.github.com>
2020-05-13 18:41:11 +0200

    Update GuiEingabePanel.java
    added comments for class and method description in javadoc style

--- src/GuiEingabePanel.java
+++ src/GuiEingabePanel.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
+
+/**
+* @class GuiUpperPanel	is a JPanel containing a vertical JSplitPane, containing JPanel panel1 (above, contains a JLabel and a JTextArea and a JButton) and JPanel panel2 (below, contains four JButtons).
+* 						also is an ActionListener for the clickable JButtons.
+*
+* the upper JTextArea serves for user input, to enter a salutation and a Name. Pressing the startButton will parse the user input and extract the gender, title, first name and surname.
+*/
 public class GuiEingabePanel extends JPanel implements ActionListener {
     private JButton startButton, editButton, saveButton, loadButton;
     private JTextArea eingabeTextArea;
@@ -33,6 +40,17 @@ public class GuiEingabePanel extends JPanel implements ActionListener {
         this.add(new JSplitPane(JSplitPane.VERTICAL_SPLIT, panel1, panel2));
     }
 
+    /**
+	* actionPerformed is executed when the user clicks at one of the buttons
+	*
+    * if the startButton is clicked, the user input is parsed to extract the gender, title, first name and surname.
+    * if the editButton is clicked, an EditInputFrame is created which allows the user to correct the input fields if not recognized correctly.
+    * if the saveButton is clicked, the current data is saved as a contact in the attached CRM-System. This requires a CRM-System to be attached correctly.
+    * if the loadButton is clicked, an existing contact can be loaded from the attached CRM-System. This requires a CRM-System to be attached correctly.
+	*
+	* @param ae	is auto-generated when the user clicks at a button in the GuiFrame
+	*		 ae	contains information about which button was clicked
+	*/
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == startButton) {
             System.out.println("GO");

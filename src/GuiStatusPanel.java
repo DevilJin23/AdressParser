7f6a1bb6055d333835829ad6f15d3228478c82a6
DevilJin23 <45466047+DevilJin23@users.noreply.github.com>
2020-05-15 01:35:53 +0200

    Update GuiStatusPanel.java

--- src/GuiStatusPanel.java
+++ src/GuiStatusPanel.java
@@ -2,13 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
-* @class GuiStatusPanel is a JPanel containing a JLabel and a JTextField. The statusTextField shows the current program status and error messages, if an error occurs.
-*/
+ * @class GuiStatusPanel is a JPanel containing a JLabel and a JTextField. The statusTextField shows the current program status and error messages, if an error occurs.
+ */
 public class GuiStatusPanel extends JPanel {
+    private JLabel statusLabel;
     private JTextField statusTextFeld;
 
     public GuiStatusPanel() {
-        JLabel statusLabel = new JLabel("Status: ");
+        statusLabel = new JLabel("Status: ");
         statusTextFeld = new JTextField("", 30);
         statusTextFeld.setEditable(false);
         this.add(statusLabel);

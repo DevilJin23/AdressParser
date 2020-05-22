920797ba6ab4cdf16c65038f5543fe21b0d93ed3
DevilJin23 <45466047+DevilJin23@users.noreply.github.com>
2020-05-22 18:26:47 +0200

    Update GuiEditInputFrame.java
    fixed getElementList Method

--- src/GuiEditInputFrame.java
+++ src/GuiEditInputFrame.java
@@ -172,12 +172,12 @@ public class GuiEditInputFrame extends javax.swing.JFrame implements ActionListe
         return -1;
     }
     
-        public List<String> getElementList(JComboBox comboBox) {
-        List<String> elementList = new ArrayList<String>();
+    public ArrayList<String> getElementList(JComboBox comboBox) {
+        ArrayList<String> elementList = new ArrayList<String>();
         for (int i = 0; i < comboBox.getItemCount(); i++) {
             elementList.add(comboBox.getItemAt(i).toString());
         }
-        return elementList
+        return elementList;
     }
 
     public void addElement(String element, JComboBox comboBox) {
